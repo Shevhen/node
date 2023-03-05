10a1474984d675e5a187ad13f2549984fbf107cf
Shevhen <Shevhen9@gmail.com>
2023-03-05 15:37:08 -0800

    lesson 2

--- app.js
+++ app.js
@@ -26,31 +26,36 @@ app.get('/users',(req, res)=>{
 
 app.get('/users/:userId',(req, res)=>{
    const userId = req.params
+
+    if(typeof users[+userId.userId-1]=== "undefined") throw new Error(res.send({"ERROR":"User not exist"}))
     const user = users[+userId.userId-1]
 
     res.json(user)
 })
 
 app.post('/users', (req, res)=>{
-    const {user, age, gender} = req.body
+    const {username, age, gender} = req.body
 
-    const newUser = {user, age, gender}
+    const newUser = {username, age, gender}
+    if(!username|| username.length<2) throw new Error(res.send({"ERROR":"name to short"}))
+    if(!age|| username.length<1) throw new Error(res.send({"ERROR":"age can't be less then 0"}))
     users.push(newUser)
 
-    app.get('/users', ()=>{
-
-    })
-
-    // fs.appendFile(path.join('users.json'), `,${JSON.stringify(newUser)}`,(err)=>{
-    //     if(err) throw new Error(err.message)
-    // })
-
+    fs.writeFile(path.join('users.json'), `${JSON.stringify(users)}`,(err)=>{
+            if(err) throw new Error(err.message)
+        })
     res.status(200).json({massege: 'user was added!'})
 })
 
 app.delete('/users/:userId', (req, res)=>{
-    const userId = req.body
-    users.splice(+userId.userId-1,1)
+    const userId = req.params
+
+    if(typeof users[+userId.userId-1]=== "undefined") throw new Error(res.send({"ERROR":"User not exist"}))
+
+    // users.splice(+userId.userId-1, 1)
+
+    delete users[+userId.userId-1]
+
 
     res.status(200).json({massege: 'user was deleted!'})
 })
