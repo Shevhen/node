1f255db60e470508db7c3fc7b77033ff0dd2bb7b
Shevhen <Shevhen9@gmail.com>
2023-03-04 16:10:38 -0800

    lesson 2

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
+// Винести базу даних в json.file, при створенні записувати туда нових юзерів через fs
+// При створенні валідацію на імія і вік, імя повинно бути більше 2 символів, вік – не менше нуля
+// На гет, пут, деліт юзерів перевірити чи такий юзер є
+const express = require('express')
 
+const [...users]= require("./users.json");
+const {urlencoded, json} = require("express");
+
+// console.log(users)
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+app.get('/foto',(req, res)=>{
+    res.send('I AM INSIDE')
+})
+
+app.get('/users',(req, res)=>{
+    res.json(users)
+})
+
+app.get('/users/:userId',(req, res)=>{
+   const userId = req.params
+    const user = users[+userId.userId-1]
+
+    res.json(user)
+})
+
+app.post('/users', (req, res)=>{
+    const body = req.body
+    users.push(body)
+
+    res.status(200).json({massege: 'user was added!'})
+})
+
+app.delete('/users/:userId', (req, res)=>{
+    const userId = req.body
+    users.splice(+userId.userId-1,1)
+
+    res.status(200).json({massege: 'user was deleted!'})
+})
+
+
+app.listen(5000,()=>{console.log('port work')})
